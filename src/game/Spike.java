341fdcd0ae3f72d0f5b11c2fd8cdcfc93bc7dde1
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2019-03-09 18:32:00 +0000

    Rename file

--- src/game/Spike.java
+++ src/game/Spike.java
@@ -8,7 +8,7 @@ public class Spike extends StaticBody {
     private static final Shape shape = new PolygonShape(
             0.487f, 0.618f, 0.803f, -0.145f, -0.75f, -0.152f, -0.441f, 0.612f);
 
-    private static final BodyImage image = new BodyImage("data/objects/spoik.png", 2.0f);
+    private static final BodyImage image = new BodyImage("data/objects/spike.png", 2.0f);
 
     // ---------------------- CONSTRUCTOR ----------------------
     public Spike(World world) {
